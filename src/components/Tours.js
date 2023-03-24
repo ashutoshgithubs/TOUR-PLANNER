528d976eacee28d082d974293935f6f96dc47e60
Ashutosh Kumar <97340498+ashutoshgithubs@users.noreply.github.com>
2023-03-24 22:53:38 +0530

    Update Tours.js

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,7 +1,7 @@
 import Card from "./Card";
 function Tours({tours,removeTour}){
     return(
-        <div className="container  " >
+        <div className="container mb-8 " >
             <div className="flex justify-center">
                 <p className="text-lg md:text-3xl font-bold m-16 border-double  border-4 border-sky-500 px-8 md:px-16 py-4 text-center whitespace-nowrap drop-shadow-lg">Tour Planner: Get the Most Out of Your Trip  &#9992;</p>
              
