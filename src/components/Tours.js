70993548ed9e2a3165f580fbbe01cea15b9dc53f
Ashutosh Kumar <97340498+ashutoshgithubs@users.noreply.github.com>
2023-03-24 22:50:32 +0530

    Update Tours.js

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -3,7 +3,7 @@ function Tours({tours,removeTour}){
     return(
         <div className="container  " >
             <div className="flex justify-center">
-                <p className="text-lg md:text-3xl font-bold m-16 border-double  border-4 border-sky-500 px-8 md:px-16 py-4 text-center whitespace-nowrap drop-shadow-lg">Tour Planner: Get the Most Out of Your Trip  &#9992;</p>
+                <p className="text-lg md:text-3xl font-bold m-16 border-double  border-4 border-sky-500 px-4 md:px-16 py-4 text-center whitespace-nowrap drop-shadow-lg">Tour Planner: Get the Most Out of Your Trip  &#9992;</p>
              
             </div>
             <div className="cards flex flex-wrap justify-center gap-4 max-w-7xl mx-auto">
